6d2a5e73eba149dc2ae965b62159037c9e750cec
Pavlo-Hapeniuk <johnybfrank@gmail.com>
2026-06-01 13:28:03 +0000

    Deploying to gh-pages from @ Pavlo-Hapeniuk/http-request-practice@c77ec04713480d85722d9675ab3c204fb39b50de 🚀

---
+++ page-2.js
@@ -0,0 +1,2 @@
+import"./assets/styles-DRctDg6l.js";console.log("Hello!!!");
+//# sourceMappingURL=page-2.js.map
---
+++ page-3.js
@@ -0,0 +1,2 @@
+import"./assets/styles-DRctDg6l.js";console.log("Hello!!!");
+//# sourceMappingURL=page-3.js.map
